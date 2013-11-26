ef9a4663fb4928a0fb8b9e790c913cf51249e102
Timothy Cochran <timothy.cochran@gmail.com>
2013-11-26 15:15:34 +0300

    filter example

--- app/js/app.js
+++ app/js/app.js
@@ -2,6 +2,7 @@ var app = angular.module('Geeknight', []);
 
 app.controller("HomeCtrl", function($scope, FilmsService) {
     $scope.person = { name: 'kevin bacon' };
+    $scope.films = FilmsService.films();
 });
 
 app.service('FilmsService', function() {
@@ -9,6 +10,8 @@ app.service('FilmsService', function() {
     this.films = function() {
         return [
             {name: 'top gun', genre: 'action', takings: 10000},
+            {name: 'highlander', genre: 'action', takings: 30000},
+            {name: 'speed', genre: 'action', takings: 30000},
             {name: 'life of brian', genre: 'comedy', takings: 20000},
             {name: 'sleepless in seattle', genre: 'romance', takings: 5000},
             {name: 'footloose', genre: 'romance', takings: 40000000}
