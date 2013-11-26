6d0242bb34ec345191cc86940fe1de56753a7da0
Timothy Cochran <timothy.cochran@gmail.com>
2013-11-26 07:50:39 +0300

    demonstrating how to poll for a build

--- app/js/app.js
+++ app/js/app.js
@@ -1,35 +1,40 @@
 var buildMonitor = angular.module('buildMonitor', []);
 
-
 buildMonitor.controller("MonitorCtrl", function($scope, buildStatusService) {
 
     $scope.build_name = 'tcochran/kampala_geek_night'
 
-    var refreshStatus = function() { 
-        buildStatusService.status($scope.build_name, function (result) {
-            $scope.build_result = result;
-        });
-    }
-
-    $scope.changeBuild = function() {
-        refreshStatus();
-    };
-
-    refreshStatus();
-    
+    var promise = buildStatusService.monitor($scope.build_name, function (result) {
+        $scope.build_result = result;
+    });
 });
 
 
 buildMonitor.service("buildStatusService", function($http, $timeout) {
     var self = this;
 
-    this.status = function(projectName, callBack) {
-
+    this.status = function(projectName) {
         var url = "https://api.travis-ci.org/repos/" + projectName + ".json"
 
-        $http({method: 'GET', url: url })
-            .success(function(data, status, headers, config) {
-                callBack(data);
-            });
+        return $http({method: 'GET', url: url })
     }
+
+    this.monitor = function (projectName, statusChangedCallBack) {
+        var lastBuildNumber = 0;
+
+        function checkStatus() {
+
+            self.status(projectName).then(function(result) {
+
+                var status = result.data; 
+                if (lastBuildNumber != status.last_build_number && status.last_build_result != null)
+                {
+                    lastBuildNumber = status.last_build_number;
+                    statusChangedCallBack(status);
+                }
+            })
+            $timeout(checkStatus, 30000);
+        };  
+        checkStatus();
+    };
 });
