74d4d668db6763252129cfa8762b5e1a2e3e109e
Timothy Cochran <timothy.cochran@gmail.com>
2013-11-26 14:51:07 +0300

    scope examples

--- app/js/app.js
+++ app/js/app.js
@@ -1,6 +1,9 @@
 var app = angular.module('Geeknight', []);
 
-app.controller("HomeCtrl", function() {
+app.controller("HomeCtrl", function($scope) {
+    $scope.person = { name: 'kevin bacon' };
+});
 
-    
+app.controller("InnerCtrl", function($scope) {
+    $scope.person.name = 'ralph lauren';
 });
