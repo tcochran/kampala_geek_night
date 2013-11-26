d781a7b363e265e4c69b405ca3bc8b3811660d53
Timothy Cochran <timothy.cochran@gmail.com>
2013-11-26 15:11:30 +0300

    film service

--- app/js/app.js
+++ app/js/app.js
@@ -1,9 +1,19 @@
 var app = angular.module('Geeknight', []);
 
-app.controller("HomeCtrl", function($scope) {
+app.controller("HomeCtrl", function($scope, FilmsService) {
     $scope.person = { name: 'kevin bacon' };
 });
 
-app.controller("InnerCtrl", function($scope) {
-    $scope.person.name = 'ralph lauren';
-});
+app.service('FilmsService', function() {
+
+    this.films = function() {
+        return [
+            {name: 'top gun', genre: 'action', takings: 10000},
+            {name: 'life of brian', genre: 'comedy', takings: 20000},
+            {name: 'sleepless in seattle', genre: 'romance', takings: 5000},
+            {name: 'footloose', genre: 'romance', takings: 40000000}
+        ];
+    }
+})
+
+
